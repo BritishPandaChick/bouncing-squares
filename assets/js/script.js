001795f3bc5485cc5722c6ed1d7aa71396ae419b
BritishPandaChick <sbartley1080@gmail.com>
2019-12-30 18:49:21 -0500

    Add JS code for update function. Change variable names for velocity and gravity

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,18 +17,25 @@ window.onload = function(){
         this.y = _y;
 
         //let's give velocity X and Y
-        this.xVel = 10;
+        this.xVel = 10 + Math.random()*20;
         this.yVel = 1;
 
         //the box width and height
         this.width = 20;
         this.height = 20;
 
+        //random colors for our box
+        this.r = Math.round(Math.random() * 255);
+        this.g = Math.round(Math.random() * 255);
+        this.b = Math.round(Math.random() * 255);
+
+        this.rgba = "rgba(" + this.r + "," + this.g + "," + this.b + ",1)";
+
         //let's make draw method for our box
         this.draw = function(){
           //make box class for easy access
-          ctx.fillStyle = "red";
-          ctx.fillRect(this.x, this.y, this.width, this.height);
+          ctx.strokeStyle = this.rgba;
+          ctx.strokeRect(this.x, this.y, this.width, this.height);
 
           this.update();
         }
@@ -55,35 +62,59 @@ window.onload = function(){
             this.yVel *= -1; //make it bounce
           }
 
+          /* boxes don't try to add y by its velocity when it reaches bottom causing it to spazz */
+          //now we add gravity
+          if(this.y < H - this.height) {
+            this.yVel += .25;
+          }
+
           //check the bottom border
           if(this.y > H - this.height){
+            //when it bounce off the bottom decrease the ball speed
+            this.xVel *= .5;
+            this.yVel *= .5;
+
             this.y = H - this.height; //set its position to 0
             this.yVel *= -1; //make it bounce
           }
 
+          //move add speed to our x/y
           this.x += this.xVel;
           this.y += this.yVel;
         }
       }
-
-      //get an object instance of our Box class
-      var b = new Box(20, 20);
+      //lets make array of boxes
+      var boxes [];
 
       //function to draw stuff on screen
       function draw(){
         //background
         ctx.globalCompositeOperation = "source-over";
-        ctx.fillStyle = "black";
+        ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
         ctx.fillRect(0,0,W,H);
 
         ctx.globalCompositeOperation = "lighter";
-        b.draw();
+
+        //loop through the boxes and draw them
+        for(i=0; i < boxes.length; i++) {
+          boxes[i].draw();
+        }
 
         update();
       }
 
       //function for logic
-      function update(){}
+      function update(){
+        //loop through the boxes and draw them
+        for(i=0; i < boxes.length; i++) {
+          boxes[i].update();
+        }
+      }
+
+      //add box every minute we specify
+      setInterval(function(){
+        boxes.push(new Box(0,0));
+      },1000);
 
       //set interval so we can draw then update our drawing
       //every 30 milliseconds
